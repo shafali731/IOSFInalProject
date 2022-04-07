3a498485826295f4bf063a2fc3f113ee1ee625e7
Shafali Gupta <shafaligupta31@gmail.com>
2022-04-07 13:46:24 -0400

    Initial Commit

---
+++ WordOfTheDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordOfTheDay
+//
+//  Created by Shafali Gupta on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
