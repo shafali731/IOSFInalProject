3e50bd54570f70cf241626bfa9482129de3379d6
Shafali Gupta <shafaligupta31@gmail.com>
2022-04-11 12:04:27 -0400

    testing first screen interface

--- WordOfTheDay/ViewController.swift
+++ WordOfTheDay/ViewController.swift
@@ -3,17 +3,31 @@
 //  WordOfTheDay
 //
 //  Created by Shafali Gupta on 4/7/22.
-// initial commit 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var randButton: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        wordLabel.text = ""
+        infoButton.isHidden = true
+        randButton.text = ""
     }
 
-
+    @IBAction func randomWord(_ sender: Any) {
+        let array = ["hello", "bye", "word", "tester"]
+        wordLabel.text = array.randomElement()
+        infoButton.isHidden = false
+    }
+    @IBAction func getInfo(_ sender: Any) {
+        randButton.text = "hi"
+    }
 }
 
