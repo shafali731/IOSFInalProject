04d87a9204f967660412a2e30975341d47f1c1b2
Shafali Gupta <shafaligupta31@gmail.com>
2022-05-11 15:43:36 -0400

    added delegate protocol

--- WordOfTheDay/FavoritesViewController.swift
+++ WordOfTheDay/FavoritesViewController.swift
@@ -8,14 +8,53 @@
 import Foundation
 import UIKit
 
+//protocol RandomFavoriteDelegate: AnyObject{
+////    func randomButtonTapped(name:String, button: UIButton, id: Int)
+//    func randomButtonTapped()
+//}
+//class RandomBarButtonItem : UIBarButtonItem {
+//    weak var delegate: RandomFavoriteDelegate?
+//    override init()     {
+////        print("uyhu")
+//        super.init()
+////        target = self
+////        action = #selector(tapped)
+//    }
+//
+//    required init?(coder aDecoder:NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
+//    override func awakeFromNib() {
+//            self.action = #selector(RandomBarButtonItem.didTapButton)
+//        }
+//
+//    @objc func didTapButton() {
+//        print("efefh")
+//        delegate?.randomButtonTapped()
+//    }
+//}
 
-class FavoritesViewController: UITableViewController {
+class FavoritesViewController: UITableViewController, RandomFavoriteDelegate {
+    var delegate : RandomFavoriteDelegate?
+    func randomButtonTapped(favWord: String) {
+        let alert = UIAlertController(
+            title: "Random Favorite",
+            message: favWord,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Get Details", style: .cancel, handler: { action -> Void in self.performSegue(
+            withIdentifier: "showFavoriteDetails",
+            sender: favWord)}))
+        present(alert, animated: true)
+    }
+    
+    @IBOutlet weak var RandButton: RandomBarButtonItem!
     var FavoritesList = [FavoritedWord]()
     let context = (UIApplication.shared.delegate as! AppDelegate) .persistentContainer.viewContext
-
-
+//    let barButton = RandomBarButtonItem()
     override func viewDidLoad() {
         super.viewDidLoad()
+        RandButton.delegate = self
         navigationItem.largeTitleDisplayMode = .never
         let darkModeEnabled = UserDefaults.standard.value(forKey: "darkModeEnabled") as? Bool
         print(darkModeEnabled!)
@@ -26,6 +65,7 @@ class FavoritesViewController: UITableViewController {
             overrideUserInterfaceStyle = .light
         }
         self.title = "Favorite Words"
+//        self.navigationItem.setLeftBarButton(barButton, animated: true)
         tableView.register(
           UITableViewCell.self,
           forCellReuseIdentifier: "FavoritedWordsCell")
@@ -158,51 +198,64 @@ class FavoritesViewController: UITableViewController {
         controller.word = sender as! String
         }
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+//extension FavoritesViewController: RandomFavoriteDelegate{
+//    delegate = self
+//    func randomButtonTapped() {
+//        print("EHFIUHFUEHIUFH")
+//        let alert = UIAlertController(
+//            title: "Random Favorite",
+//            message: "here you go",
+//            preferredStyle: .alert
+//        )
+//        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//        present(alert, animated: true)
+//    }
+//}
+
+/*
+// Override to support conditional editing of the table view.
+override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+}
+*/
+
+/*
+// Override to support editing the table view.
+override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+        // Delete the row from the data source
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+}
+*/
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+}
+*/
---
+++ WordOfTheDay/RandomButtonController.swift
@@ -0,0 +1,42 @@
+//
+//  RandomButtonController.swift
+//  WordOfTheDay
+//
+//  Created by Shafali Gupta on 5/11/22.
+//
+
+import Foundation
+import UIKit
+
+protocol RandomFavoriteDelegate: AnyObject{
+//    func randomButtonTapped(name:String, button: UIButton, id: Int)
+    func randomButtonTapped(favWord: String)
+}
+class RandomBarButtonItem : UIBarButtonItem {
+    weak var delegate: RandomFavoriteDelegate?
+    let context = (UIApplication.shared.delegate as! AppDelegate) .persistentContainer.viewContext
+    override init()     {
+        super.init()
+    }
+
+    required init?(coder aDecoder:NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    override func awakeFromNib() {
+            self.action = #selector(RandomBarButtonItem.didTapButton)
+        }
+
+    @objc func didTapButton() {
+        var word: String
+        do{
+            var FavoList = [FavoritedWord]()
+            FavoList = try context.fetch(FavoritedWord.fetchRequest())
+            word = (FavoList.randomElement()!).name!
+            delegate?.randomButtonTapped(favWord: word)
+        }
+        catch{
+            print("Could not load Favorites")
+        }
+    }
+}
+
